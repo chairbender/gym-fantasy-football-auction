2b0fe042231a842297e30adb0fcfb171a5a4dd02
kwhipke1 <kwhipke1@gmail.com>
2017-12-29 04:56:16 -0800

    fix setup

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup
 setup(name='gym_fantasy_football_auction',
       version='0.0.1',
       install_requires=['gym>=0.7.4', 'fantasy_football_auction>=0.9.5', 'six>=1.11.0'],
-      packages=['gym_fantasy_football_auction.envs'],
-      package_dir={'gym_fantasy_football_auction.envs': 'gym_fantasy_football_auction/envs'},
+      packages=['gym_fantasy_football_auction', 'gym_fantasy_football_auction.envs'],
       package_data={'gym_fantasy_football_auction.envs': ['data/*.csv']},
       )
